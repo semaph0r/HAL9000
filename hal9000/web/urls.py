7deb9655e6251b2b972b9ffe240832f664fdd633
Patrik Schmidt <patrik@visionhack.de>
2020-12-06 15:34:20 +0100

    [impl] add CRUD for certificates

--- hal9000/web/urls.py
+++ hal9000/web/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.show, name='index'),
-    path('show', views.show),
-    path('edit/<int:id>', views.edit),
-    path('update/<int:id>', views.update),
-    path('delete/<int:id>', views.destroy),
+    path('certificates', views.CertificatesListView.as_view(), name='show-certificate'),
+    path('certificate/create', views.CertificateCreateView.as_view(), name='create-certificate'),
+    path('certificate/<int:pk>/update', views.CertificateUpdateView.as_view(), name='update-certificate'),
+    path('certificate/<int:pk>/delete', views.CertificateDeleteView.as_view(), name='delete-certificate'),
 ]
--- hal9000/web/views.py
+++ hal9000/web/views.py
@@ -1,7 +1,39 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .forms import CertificateForm
 from .models import Certificate
 from django.http import HttpResponse
+
+
+class CertificatesListView(ListView):
+    model = Certificate
+
+
+class CertificateCreateView(CreateView):
+    model = Certificate
+    fields = [
+        'domain',
+        'maintainer',
+        'send_notification',
+    ]
+    success_url = reverse_lazy('index')
+
+
+class CertificateUpdateView(UpdateView):
+    model = Certificate
+    fields = [
+        'domain',
+        'maintainer',
+        'send_notification',
+    ]
+    success_url = reverse_lazy('index')
+
+
+class CertificateDeleteView(DeleteView):
+    model = Certificate
+    success_url = reverse_lazy('index')
 
 
 def show(request):
@@ -22,9 +54,3 @@ def update(request, id):
         form.save()
         return redirect("/show")
     return render(request, 'edit.html', {'certificate': certificate})
-
-
-def destroy(request, id):
-    certificate = Certificate.objects.get(id=id)
-    certificate.delete()
-    return redirect("/show")
